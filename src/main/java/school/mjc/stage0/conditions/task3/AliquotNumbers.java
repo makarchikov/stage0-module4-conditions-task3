7bc5ab4a6e4a008ae65da62900dc4a7adf600149
salvadordali99 <152178723+salvadordali99@users.noreply.github.com>
2024-08-13 19:59:19 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        int sum = 0;
+        for (int i = 1; i < first / 2 ; i++) {
+            if (first % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == second) {
+            System.out.println("Aliquot");
+        } else {
+            System.out.println("Not aliquot");
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
+            return;
+        }
+
+        // Define the number of days in each month
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println(daysInMonth[month-1]);
+
     }
 }
